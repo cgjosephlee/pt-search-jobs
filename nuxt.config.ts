790b7b43115eb68a5255a1b5701471fd11c61b94
Joseph Lee <cgjosephlee@gmail.com>
2024-11-11 09:45:10 +0000

    fix: disable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   nitro: { preset: 'vercel-edge' },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
