4f776c1b39a94435c83911f313f9b87f81e187a0
Joseph Lee <cgjosephlee@gmail.com>
2024-11-12 08:57:42 +0000

    fix: favicon config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   routeRules: {
     '/': { prerender: true }
   },
+  app: { head: { link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }] } },
   nitro: { preset: 'vercel-edge' },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
